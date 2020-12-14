7eb848f74ff74148af944264e9dfa10534134e3c
Sahil Pabale <dev.sahilpabale@gmail.com>
2020-12-14 09:46:00 +0530

    updated database env variables

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
-const host = process.env.LOCAL_DB_HOST;
-const user = process.env.LOCAL_DB_USER;
-const password = process.env.LOCAL_DB_PASSWORD;
-const database = process.env.LOCAL_DB;
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
 
 // using knex
 const db = require("knex")({
